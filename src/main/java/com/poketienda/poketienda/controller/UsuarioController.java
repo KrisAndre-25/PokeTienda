12da1b99cac39359c928fcb453aaf6abb70c3c03
PAAWSA02LC1327 <Duoc@PAAWSA02LC1327.duoc.acad>
2026-04-27 17:04:57 -0400

    feature: se agregan metodos POST, PUT y DELETE al controlador de usuarios

--- src/main/java/com/poketienda/poketienda/controller/UsuarioController.java
+++ src/main/java/com/poketienda/poketienda/controller/UsuarioController.java
@@ -1,31 +1,33 @@
 package com.poketienda.poketienda.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.poketienda.poketienda.model.Usuario;
 import com.poketienda.poketienda.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
 public class UsuarioController {
-
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
-    public List<Usuario> listarUsuarios() {
+    public List<Usuario> listarUsuarios(){
         return usuarioService.getUsuarios();
     }
 
     @PostMapping
     public Usuario crear(@RequestBody Usuario usuario) {
-        return usuarioService.crearUsuario(usuario);
+        return usuarioService.saveUsuario(usuario);
     }
 
     @PutMapping("/{id}")
     public Usuario actualizar(@PathVariable int id, @RequestBody Usuario usuario) {
-        return usuarioService.actualizarUsuario(id, usuario);
+        usuario.setId(id); // Aseguramos que el ID sea el correcto
+        return usuarioService.updateUsuario(usuario);
     }
 
     @DeleteMapping("/{id}")
